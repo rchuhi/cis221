04550962c9511d4bc951764fa12c0530295aaa65
rchuhi <94059813+rchuhi@users.noreply.github.com>
2022-02-03 14:28:33 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*Reginah Chuhi
+CIS221
+Tuesday and Thursday 2:10pm*/
+
+
+
+	
+// Creates a Main class
+public class Main {
+  int x=3;
+  int y=7; 
+
+  
+}
---
+++ Second.java
@@ -0,0 +1,14 @@
+
+public class Second {
+
+	// Creates a Main class
+		  public static void main(String[] args) {
+		    Main myObj = new Main();
+		    //calls the variables from Main class
+		    System.out.println(myObj.x * myObj.y);
+		  }
+		
+
+	}
+
+
